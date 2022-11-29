cc9c4d8a8bb45527da340e726ebfe30d4b7fb821
Daniel King <king.daniel.thomas@gmail.com>
2022-11-29 14:16:10 -0500

    Create getComputerChoice function
    Assign the random choice to variable compThrow

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
 let throwOptions = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+  const compThrow = throwOptions[Math.floor(Math.random() * throwOptions.length)];
+  console.log(compThrow);
+}
