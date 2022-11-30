aa93e4a9ad1e995561f08314c4aaac31d4a51444
Daniel King <king.daniel.thomas@gmail.com>
2022-11-30 17:47:31 -0500

    Use a forEach loop on all buttons to instantly access their Attributes

--- scripts.js
+++ scripts.js
@@ -4,11 +4,33 @@ function getComputerThrow(){
   return throwOptions[Math.floor(Math.random() * throwOptions.length)];
 }
 
-const playerThrow = "rock";
+// const playerThrow = "rock";
+
+/* Try #1 - learned that I should be using a for loop.
+let rock = document.getElementById("rockid");
+let paper = document.getElementById("paperid");
+let scissors = document.getElementById("scissorsid");
+
+rock.addEventListener("click", playerThrow);
+paper.addEventListener("click", playerThrow);
+scissors.addEventListener("click", playerThrow);
+
+function playerThrow(){
+  return rock.getAttribute('data-element');
+};
+*/
+
+let btns = document.querySelectorAll('button');
+
+btns.forEach(function (i) {
+  i.addEventListener('click', function(){
+    i.getAttribute('data-element');
+  });
+});
 
 function playRound(player, computer){
 
-  player = playerThrow;
+  player = btns;
   computer = getComputerThrow();
   console.log(computer);
 
