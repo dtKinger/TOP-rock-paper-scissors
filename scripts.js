497c03dcc7bc465ce0da6c9bcac25b57c643333f
Daniel King <king.daniel.thomas@gmail.com>
2022-11-29 20:44:16 -0500

    Passes local argument instead of global variable

--- scripts.js
+++ scripts.js
@@ -4,21 +4,41 @@ function getComputerThrow(){
   return throwOptions[Math.floor(Math.random() * throwOptions.length)];
 }
 
-const playerThrow = "rock";
+// A function to quickly test game alert messages //
+function ranPlayerThrow(){
+  return throwOptions[Math.floor(Math.random() * throwOptions.length)];
+}
+// const playerThrow = "rock";
 
 function playRound(player, computer){
 
-  player = playerThrow;
+  player = ranPlayerThrow();
   computer = getComputerThrow();
   console.log(computer);
 
-  if (playerThrow == "rock"){
+  if (player == "rock"){
     if (computer == "scissors"){
-      alert("Rock beats scissors, You won!");
+      alert("YOUR ROCK smashes the scissors to pieces!");
     } else if (computer == "paper" ) {
-      alert("The Computer beat you with paper!");
+      alert("COMPUTER'S PRINTER PAPER smothers your pet rock!");
     } else {
-      alert("It's a tie game!");
+      alert("DRAW!");
+    }
+  } else if (player == "paper"){
+    if (computer == "scissors"){
+      alert("COMPUTER SCISSOR HANDS dices you up!");
+    } else if (computer == "rock" ) {
+      alert("YOU SMOTHER the computer's pet rock!");
+    } else {
+      alert("DRAW!");
+    }
+  } else if (player == "scissors"){
+    if (computer == "paper"){
+      alert("YOU absolutely SHRED what they're printing out!");
+    } else if (computer == "rock" ) {
+      alert("Your scissors are now scissorses... they've been smashed to pieces by ELECTRONIC ROCK!");
+    } else {
+      alert("DRAW!");
     }
   }   
 };
