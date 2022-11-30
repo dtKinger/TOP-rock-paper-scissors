f7bb60aabd491cb634cadfc2a66594a54bce75ac
Daniel King <king.daniel.thomas@gmail.com>
2022-11-29 20:19:11 -0500

    Uses a fixed input for playerThrow to playRound()

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
-let throwOptions = ["rock", "paper", "scissors"];
+const throwOptions = ["rock", "paper", "scissors"];
 
-function getComputerChoice(){
-  const compThrow = throwOptions[Math.floor(Math.random() * throwOptions.length)];
-  console.log(compThrow);
+function getComputerThrow(){
+  return throwOptions[Math.floor(Math.random() * throwOptions.length)];
 }
+
+const playerThrow = "rock";
+
+function playRound(player, computer){
+
+  player = playerThrow;
+  computer = getComputerThrow();
+  console.log(computer);
+
+  if (playerThrow == "rock"){
+    if (computer == "scissors"){
+      alert("Rock beats scissors, You won!");
+    } else if (computer == "paper" ) {
+      alert("The Computer beat you with paper!");
+    } else {
+      alert("It's a tie game!");
+    }
+  }   
+};
